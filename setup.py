13b84cc555cffd864db4ae71a0850dadaf3ed5a0
Robert Kiewisz <56911280+RRobert92@users.noreply.github.com>
2022-03-23 12:55:11 +0100

    Initial project setup

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from tardis.version import version
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+setup(
+    author=["Robert Kiewisz", "Tristan Bepler"],
+    author_email='rkiewisz@nysbc.com',
+    python_requires='>=3.8',
+    requirements=[],
+    classifiers=[
+        'Development Status :: Drafting Alpha Release',
+        'Intended Audience :: Developers/Research',
+        'Environment :: Console/WebApp',
+        'Environment :: GPU :: NVIDIA CUDA :: 11.1',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="PyTorch segmentation of 2D/3D images such as electron tomography (ET), Cryo-EM or "
+                "fluorescent microscopy data into 3D segmented point cloud.",
+    entry_points={
+        'console_scripts': [],
+    },
+    license="MIT License",
+    long_description_content_type='text/x-rst',
+    long_description=readme,
+    include_package_data=True,
+    keywords=['spindletorch', 'semantic segmentation', 'point cloud segmentation',
+              'MT segmentation', 'UNet', 'Unet3Plus'],
+    name='tardis',
+    longname='Transformer And Rapid Dimensionless Instance Segmentation',
+    packages=find_packages(include=['tardis']),
+    url='https://github.com/SMLC-NYSBC/tardis',
+    version=version,
+)
