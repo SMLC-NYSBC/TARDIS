629a2eebcf096d3d6f253470f600f0b1c4f77335
Robert Kiewisz <56911280+RRobert92@users.noreply.github.com>
2022-03-24 15:47:03 +0100

    slcpy - 2D label drawing for 2D/3D data set

---
+++ tardis/slcpy_data_processing/build_semantic_mask.py
@@ -0,0 +1,62 @@
+import numpy as np
+from tardis.slcpy_data_processing.utilis.draw_mask_2D import draw_2D
+from tardis.slcpy_data_processing.utilis.interpolation import interpolation_3D
+
+
+def slcpy_semantic(mask_size: tuple,
+                   coordinate: np.ndarray,
+                   pixel_size: float,
+                   circle_size=250,
+                   multi_layer=False,
+                   tqdm=True):
+    """
+    MODULE TO BUILD SEMANTIC MASK FROM CORRESPONDING COORDINATES
+    
+    Args:
+        mask_size: Size of array that will hold created mask
+        coordinate: Segmented coordinates of a shape [Label x X x Y x Z]
+        pixel_size: Pixel size in Angstrom
+        circle_size: Size of a circle the label mask in Angstrom
+        multi_layer: single, or unique value for each lines
+        tqdm: If True build with progress bar
+    """
+    assert coordinate.ndim == 2 and coordinate.shape[1] == 4, \
+        'Included coordinate array is not of a correct shape.'
+        
+    label_mask = np.zeros(mask_size)
+    r = round((circle_size / 2) / pixel_size)
+    
+    if multi_layer:
+        label_mask = np.stack((label_mask,) * 3, axis=-1)
+    else:
+        segment_color = [1]
+        
+    segments = np.unique(coordinate[:, 0]) # Number of segments in coordinates
+    
+    if tqdm:
+        from tqdm import tqdm
+        
+        batch_iter = tqdm(range(len(segments)),
+                        'Building semantic mask',
+                        total=len(segments),
+                        leave=False)
+    else:
+        batch_iter = range(len(segments))
+        
+    # Build semantic mask by drawing circle in 2D for each coordinate point
+    for i in batch_iter:
+        points = coordinate[np.where(coordinate[:, 0] == i)[0]][:, 1:]
+        
+        label = interpolation_3D(points)
+        
+        if multi_layer:
+            segment_color = list(np.random.choice(range(256), size=3))
+        
+        # Draw 2D label
+        for j in range(len(label)):
+            c = label[j, :] # Point center
+            label_mask = draw_2D(r=r,
+                                 c=c,
+                                 label_mask=label_mask,
+                                 segment_color=segment_color)
+    return label_mask
---
+++ tests/test_label.py
@@ -0,0 +1,91 @@
+import numpy as np
+from tardis.slcpy_data_processing.build_semantic_mask import slcpy_semantic
+
+
+class TestLabelDrawing:
+    pixel_size = 2.32
+    r = round(12.5 * pixel_size)
+
+    def test_drawing_2D_single(self):
+        df_coord = np.zeros((100, 4))
+        start = 0
+        stop = 10
+
+        for i in range(10):
+            start += 10
+            stop += 10
+            df_coord[start:stop, 0] = i
+            df_coord[start:stop, 1] = i + 4
+            df_coord[start:stop, 2] = i + 2
+
+        labels_2D = slcpy_semantic(mask_size=(64, 64),
+                                   coordinate=df_coord,
+                                   pixel_size=self.pixel_size,
+                                   circle_size=self.r,
+                                   multi_layer=False,
+                                   tqdm=True)
+        assert np.any(labels_2D == 1), 'Label was not created!'
+        assert not np.any(labels_2D > 1), 'MultiLable was created in single mode!'
+    
+    def test_drawing_2D_multi(self):
+        df_coord = np.zeros((100, 4))
+        start = 0
+        stop = 10
+
+        for i in range(10):
+            start += 10
+            stop += 10
+            df_coord[start:stop, 0] = i
+            df_coord[start:stop, 1] = i + 4
+            df_coord[start:stop, 2] = i + 2
+            df_coord[start:stop, 3] = start / 2
+
+        labels_2D = slcpy_semantic(mask_size=(64, 64),
+                                   coordinate=df_coord,
+                                   pixel_size=self.pixel_size,
+                                   circle_size=self.r,
+                                   multi_layer=True,
+                                   tqdm=True)
+        assert np.any(labels_2D > 0), 'MultiLable was not created!'
+         
+    def test_drawing_3D_single(self):
+        df_coord = np.zeros((100, 4))
+        start = 0
+        stop = 10
+
+        for i in range(10):
+            start += 10
+            stop += 10
+            df_coord[start:stop, 0] = i
+            df_coord[start:stop, 1] = i + 4
+            df_coord[start:stop, 2] = i + 2
+
+        labels_3D = slcpy_semantic(mask_size=(64, 64, 64),
+                                   coordinate=df_coord,
+                                   pixel_size=self.pixel_size,
+                                   circle_size=self.r,
+                                   multi_layer=False,
+                                   tqdm=True)
+        assert np.any(labels_3D == 1), 'Label was not created!'
+        assert not np.any(labels_3D > 1), 'MultiLable was created in single mode!'
+        
+    def test_drawing_3D_multi(self):
+        df_coord = np.zeros((100, 4))
+        start = 0
+        stop = 10
+
+        for i in range(10):
+            start += 10
+            stop += 10
+            df_coord[start:stop, 0] = i
+            df_coord[start:stop, 1] = i + 4
+            df_coord[start:stop, 2] = i + 2
+            df_coord[start:stop, 3] = start / 2
+
+        labels_3D = slcpy_semantic(mask_size=(64, 64, 64),
+                                   coordinate=df_coord,
+                                   pixel_size=self.pixel_size,
+                                   circle_size=self.r,
+                                   multi_layer=True,
+                                   tqdm=True)
+        assert np.any(labels_3D > 0), 'MultiLable was not created!'
