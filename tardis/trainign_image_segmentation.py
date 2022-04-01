a73908b79d8f9fe3f7f2c4b07148e315bf3a6571
Robert Kiewisz <56911280+RRobert92@users.noreply.github.com>
2022-04-01 17:35:57 +0200

    file renaming

--- tardis/trainign_image_segmentation.py
+++ tardis/trainign_image_segmentation.py
@@ -1,16 +1,111 @@
-from os import listdir, mkdir
+from os import listdir, mkdir, getcwd
 from os.path import isdir, join
 from shutil import rmtree
 from typing import Optional
 
+import click
+from tardis.spindletorch.utils.device import get_device
 from torch.utils.data import DataLoader
 
-from tardis.slcpy_data_processing.build_training_dataset import BuildTestDataSet, BuildTrainDataSet
+from tardis.slcpy_data_processing.build_training_dataset import BuildTestDataSet, \
+    BuildTrainDataSet
 from tardis.spindletorch.train import train
 from tardis.spindletorch.utils.dataset_loader import VolumeDataset
+from tardis.version import version
 
 
+@click.command()
+@click.option('-dir', '--training_dataset',
+              default=getcwd(),
+              type=str,
+              help='Directory with train, test folder or folder with dataset '
+              'to be used for training.',
+              show_default=True)
+@click.option('-dir', '--train_test_ratio',
+              default=10,
+              type=float,
+              help='Percentage value of train dataset that will become test.',
+              show_default=True)
+@click.option('-cnn', '--cnn_type',
+              default=64,
+              type=click.Choice(['unet', 'resunet', 'unet3plus'],
+                                case_sensitive=True),
+              help='Type of NN used for training.',
+              show_default=True)
+@click.option('-co', '--cnn_out_channel',
+              default=1,
+              type=int,
+              help='Number of output channels for the NN.',
+              show_default=True)
+@click.option('-b', '--taining_batch_size',
+              default=25,
+              type=int,
+              help='Batch size.',
+              show_default=True)
+@click.option('-cl', '--cnn_layers',
+              default=5,
+              type=int,
+              help='Number of convolution layer for NN.',
+              show_default=True)
+@click.option('-cm', '--cnn_multiplayer',
+              default=5,
+              type=int,
+              help='Convolution multiplayer for CNN layers.',
+              show_default=True)
+@click.option('-l', '--cnn_loss',
+              default=click.Choice(['bce', 'dice', 'hybrid', 'adaptive_dice'],
+                                   case_sensitive=True),
+              type=str,
+              help='Loss function use for training.',
+              show_default=True)
+@click.option('-la', '--loss_alpha',
+              default=None,
+              type=float,
+              help='Value of alpha used for adaptive dice loss.',
+              show_default=True)
+@click.option('-lr', '--loss_lr_rate',
+              default=0.001,
+              type=float,
+              help='Learning rate for NN.',
+              show_default=True)
+@click.option('-lrs', '--lr_rate_schedule',
+              default=False,
+              type=bool,
+              help='If True learning rate scheduler is used.',
+              show_default=True)
+@click.option('-d', '--device',
+              default=click.Choice(['gpu', 'cpu', int],
+                                   case_sensitive=True),
+              type=[str, int],
+              help='Define which device use for training: '
+              'gpu: Use ID 0 gpus'
+              'cpu: Usa CPU'
+              '0-9 - specified gpu device id to use',
+              show_default=True)
+@click.option('-e', '--epochs',
+              default=100,
+              type=int,
+              help='Number of epoches.',
+              show_default=True)
+@click.option('-es', '--early_stop',
+              default=10,
+              type=int,
+              help='Number of epoches without improvement after which early stop '
+              'is initiated.',
+              show_default=True)
+@click.option('-cch', '--cnn_checkpoint',
+              default=None,
+              type=str,
+              help='If indicated, dir to training checkpoint to reinitialized trainign.',
+              show_default=True)
+@click.option('-dr', '--dropout_rate',
+              default=None,
+              type=float,
+              help='If indicated, value of dropout for CNN.',
+              show_default=True)
+@click.version_option(version=version)
 def main(training_dataset: str,
+         train_test_ratio: float,
          image_size: int,
          cnn_type: str,
          cnn_out_channel: int,
@@ -23,7 +118,7 @@ def main(training_dataset: str,
          lr_rate_schedule: bool,
          device: str,
          epochs: int,
-         train_test_ratio: Optional[int] = 10,
+         early_stop: int,
          loss_alpha: Optional[float] = None,
          cnn_checkpoint: Optional[str] = None,
          dropout_rate: Optional[float] = None):
@@ -114,6 +209,9 @@ def main(training_dataset: str,
                          num_workers=8,
                          pin_memory=True)
 
+    """Get device"""
+    device = get_device(device)
+
     """Run Training loop"""
     train(train_dataloader=train_DL,
           test_dataloader=test_DL,
@@ -130,7 +228,8 @@ def main(training_dataset: str,
           learning_rate=loss_lr_rate,
           learning_rate_scheduler=lr_rate_schedule,
           device=device,
-          epochs=epochs)
+          epochs=epochs,
+          early_stop_rate=early_stop)
 
 
 if __name__ == '__main__':
