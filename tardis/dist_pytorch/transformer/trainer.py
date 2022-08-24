e06e228c7f146ca77487f6ff38ae336ce4dbc9e6
Robert Kiewisz <56911280+RRobert92@users.noreply.github.com>
2022-08-24 20:57:28 +0200

    add gpu meemory count

--- tardis/dist_pytorch/transformer/trainer.py
+++ tardis/dist_pytorch/transformer/trainer.py
@@ -115,15 +115,16 @@ class Trainer:
 
             if self.device.type == 'cuda':
                 import nvidia_smi
-                
+
                 nvidia_smi.nvmlInit()
                 handle = nvidia_smi.nvmlDeviceGetHandleByIndex(self.device.index)
                 info = nvidia_smi.nvmlDeviceGetMemoryInfo(handle)
                 self.gpu_info = "Device {}: {}, Memory : {:.2f}% free".format(self.device.index,
-                                                                            nvidia_smi.nvmlDeviceGetName(handle),
-                                                                            100 * info.free / info.total)
+                                                                              nvidia_smi.nvmlDeviceGetName(handle),
+                                                                              100 * info.free / info.total)
                 nvidia_smi.nvmlShutdown()
-            else:self.gpu_info = " "
+            else:
+                self.gpu_info = " "
 
             self.progress_epoch(title='DIST training module',
                                 text_1=self.print_setting[0],
