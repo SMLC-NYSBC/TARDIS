bugfix in sofF1 cosing wronge backpropagation

tardis/dist_pytorch/transformer/losses.py
@@ -1,3 +1,4 @@
+from cmath import log
 from typing import Optional
 
 import torch
@@ -71,9 +72,10 @@ class DiceLoss(nn.Module):
 
 
 class BCELoss(nn.Module):
-    def __init__(self):
+    def __init__(self,
+                 weight: Optional[float] = None):
         super(BCELoss, self).__init__()
-        self.loss = nn.BCEWithLogitsLoss()
+        self.loss = nn.BCEWithLogitsLoss(weight=weight)
 
     def forward(self,
                 logits: torch.Tensor,
@@ -82,61 +84,37 @@ class BCELoss(nn.Module):
         return self.loss(logits, targets)
 
 
-class SoftF1(nn.Module):
-    def __init__(self,
-                 grad: bool):
+class SoftF1:
+    def __init__(self):
         super(SoftF1, self).__init__()
-        self.grad = grad
 
-    def forward(self,
+    def __call__(self,
                 logits: torch.Tensor,
                 targets: torch.Tensor):
-        if self.grad:
-            logits = logits.flatten()
-            targets = targets.flatten()
+        with torch.no_grad():
+            logits = torch.sigmoid(logits)
+            logits = logits.flatten().cpu().detach().numpy()
+            targets = targets.flatten().cpu().detach().numpy()
 
-            tp = (logits * targets).sum(dim=0)
-            fp = (logits * (1 - targets)).sum(dim=0)  # soft
-            fn = ((1 - logits) * targets).sum(dim=0)  # soft
-            tn = ((1 - logits) * (1 - targets)).sum(dim=0)  # soft
+            tp = (logits * targets).sum()
+            fp = (logits * (1 - targets)).sum()  # soft
+            fn = ((1 - logits) * targets).sum()  # soft
+            tn = ((1 - logits) * (1 - targets)).sum()  # soft
 
-            soft_f1_class1 = 2 * tp / (2 * tp + fn + fp + 1e-16)
-            soft_f1_class0 = 2 * tn / (2 * tn + fn + fp + 1e-16)
+            """Accuracy Score"""
+            accuracy = (tp + tn) / (tp + tn + fp + fn + 1e-8)
 
-            cost_class1 = 1 - soft_f1_class1
-            cost_class0 = 1 - soft_f1_class0
+            """Precision Score - tp / (tp + fp)"""
+            precision_score_1 = 1 - (tp / (tp + fp + 1e-8))
+            precision_score_0 = 1 - (tn / (tn + fp + 1e-8))
+            precision = 0.5 * (precision_score_1 + precision_score_0)
 
-            return 0.5 * (cost_class1 + cost_class0)
-        else:
-            with torch.no_grad():
-                logits = logits.flatten()
-                targets = targets.flatten()
+            """Recall Score - tp / (tp + tn)"""
+            recall_score_1 = 1 - (tp / (tp + fn + 1e-8))
+            recall_score_0 = 1 - (tn / (tn + fn + 1e-8))
+            recall = 0.5 * (recall_score_1 + recall_score_0)
 
-                tp = (logits * targets).sum(dim=0)
-                fp = (logits * (1 - targets)).sum(dim=0)  # soft
-                fn = ((1 - logits) * targets).sum(dim=0)  # soft
-                tn = ((1 - logits) * (1 - targets)).sum(dim=0)  # soft
+            """F1 Score """
+            f1 = 2 * ((precision * recall) / (precision + recall + 1e-8))
 
-                """Accuracy Score"""
-                accuracy = (tp + tn) / (tp + tn + fp + fn + 1e-8)
-
-                """Precision Score - tp / (tp + fp)"""
-                precision_score_1 = 1 - (tp / (tp + fp + 1e-8))
-                precision_score_0 = 1 - (tn / (tn + fp + 1e-8))
-                precision = 0.5 * (precision_score_1 + precision_score_0)
-
-                """Recall Score - tp / (tp + tn)"""
-                recall_score_1 = 1 - (tp / (tp + fn + 1e-8))
-                recall_score_2 = 1 - (tn / (tn + fn + 1e-8))
-                recall = 0.5 * (recall_score_1 + recall_score_2)
-
-                """F1 Score """
-                soft_f1_class1 = 2 * tp / (2 * tp + fn + fp + 1e-16)
-                soft_f1_class0 = 2 * tn / (2 * tn + fn + fp + 1e-16)
-
-                cost_class1 = 1 - soft_f1_class1
-                cost_class0 = 1 - soft_f1_class0
-                f1 = 0.5 * (cost_class1 + cost_class0)
-
-            return accuracy.cpu().detach().numpy(), precision.cpu().detach().numpy(), \
-                recall.cpu().detach().numpy(), f1.cpu().detach().numpy()
+        return accuracy, precision, recall, f1