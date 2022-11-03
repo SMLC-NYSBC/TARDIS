e5713195e2de630e6b9cef50241d06d3c95b7074
Robert Kiewisz <56911280+RRobert92@users.noreply.github.com>
2022-11-03 17:46:19 +0100

    Fixed aws update

--- tardis/utils/aws.py
+++ tardis/utils/aws.py
@@ -1,121 +1,123 @@
 import io
-from os import mkdir
+import json
+from os import makedirs, mkdir
 from os.path import expanduser, isdir, isfile, join
 from typing import Optional
 
 import requests
-from tardis.utils.utils import MD5Count
+
+from tardis.utils.errors import TardisError
 
 
 def get_weights_aws(network: str,
                     subtype: str,
-                    model: Optional[str] = ''):
+                    model: Optional[str] = None):
     """
     Module to download pre-train weights from S3 AWS bucket.
+
+    Model weight stored on S3 bucket with the naming convention
+    network_subtype/model/model_weights.pth
+    ege.:
+        - fnet_32/microtubules/model_weights.pth
+        - dist_triang/microtubules/model_weights.pth
+
+    Weights are stored in ~/.tardis_pytorch with the same convention and .txt
+    file with file header information to identified update status for local file
+    if the network connection can be establish.
 
     Args:
         network (str): Type of network for which weights are requested.
         subtype (str): Sub-name of the network or sub-parameter for the network.
         model (str): Additional dataset name used for the DIST.
-        save_weights (bool): If the True model is saved in the temp repository.
     """
     """Get weights for CNN"""
-    if network in ['unet', 'unet3plus', 'fnet']:
-        assert subtype in ['16', '32', '64'], \
-            'For TARDIS, pre train model must be selected correctly!'
+    dir = join(expanduser('~'), '.tardis_pytorch', f'{network}_{subtype}', f'{model}')
 
-        dir = join(expanduser('~'), '.tardis_pytorch', f'{network}_{subtype}')
-        if aws_check_with_temp(model_name=f'{network}/{subtype}'):
-            return join(dir, 'model_weights.pth')
-        else:
-            weight = requests.get(
-                f'https://tardis-weigths.s3.amazonaws.com/{network}_{subtype}/model_weights.pth'
-            )
-    elif network == 'dist':
-        """Get weights for DIST"""
-        assert model in ['cryo_membrane', 'microtubules'], \
-            'For DIST, pre train model must be selected!'
-        assert subtype in ['with_img', 'without_img'], \
-            'For DIST, pre train subtype of model must be selected!'
+    assert network in ['unet', 'unet3plus', 'fnet', 'dist'], \
+        TardisError('aws',
+                    'tardis/utils/aws.py',
+                    f'Incorrect CNN network selected {network}_{subtype}')
+    assert subtype in ['16', '32', '64', '96', '128', 'triang', 'full'], \
+        TardisError('aws',
+                    'tardis/utils/aws.py',
+                    f'Incorrect CNN subtype selected {network}_{subtype}')
 
-        dir = join(expanduser('~'), '.tardis_pytorch', f'{network}_{model}_{subtype}')
-        if aws_check_with_temp(model_name=f'{network}/{model}/{subtype}'):
-            return join(dir, 'model_weights.pth')
-        else:
-            weight = requests.get(
-                f'https://tardis-weigths.s3.amazonaws.com/{network}/{model}/{subtype}/model_weights.pth'
-            )
+    assert model in ['microtubules', 'cryo_mem'], \
+        TardisError('aws',
+                    'tardis/utils/aws.py',
+                    f'Incorrect CNN model selected {model}')
+
+    if aws_check_with_temp(model_name=[network, subtype, model]):
+        return join(dir, 'model_weights.pth')
+    else:
+        weight = requests.get(
+            f'https://tardis-weigths.s3.amazonaws.com/{network}_{subtype}/'
+            f'{model}/model_weights.pth'
+        )
 
     """Save temp weights"""
     if not isdir(join(expanduser('~'), '.tardis_pytorch')):
         mkdir(join(expanduser('~'), '.tardis_pytorch'))
 
     if not isdir(dir):
-        mkdir(dir)
+        makedirs(dir)
 
-    open(join(dir, 'model_weights.pth'), 'wb').write(weight.content)
+    open(join(dir, 'model_weights.pth'), 'wb').write(weight.content)  # Save weights
+    with open(join(dir, 'model_header.json'), 'w') as f:
+        json.dump(dict(weight.headers), f)  # Save header
+
     print(f'Pre-Trained model download from S3 and saved/updated in {dir}')
 
     return io.BytesIO(weight.content)
 
 
-def aws_check_with_temp(model_name: str) -> bool:
+def aws_check_with_temp(model_name: list) -> bool:
     """Check if temp dir exist"""
     if not isdir(join(expanduser('~'), '.tardis_pytorch')):
-        return False
+        return False  # No weight, first Tardis run, download from aws
 
-    """Check MD5 for stored file in ~/tardis_pytorch/..."""
-    save_md5 = None
-    m = model_name.split('/')
+    """Check for stored file header in ~/tardis_pytorch/..."""
+    save = None
 
-    if len(m) == 2:
+    if not isfile(join(expanduser('~'),
+                       '.tardis_pytorch',
+                       f'{model_name[0]}_{model_name[1]}',
+                       f'{model_name[2]}',
+                       'model_weights.pth')):
+        return False  # Define network was never used with tardis, download from aws
+    else:
         if not isfile(join(expanduser('~'),
                            '.tardis_pytorch',
-                           f'{m[0]}_{m[1]}',
-                           'model_weights.pth')):
-            return False
+                           f'{model_name[0]}_{model_name[1]}',
+                           f'{model_name[2]}',
+                           'model_header.json')):
+            return False  # Weight found but no json, download from aws
         else:
-            md5 = MD5Count(open(join(expanduser('~'),
-                                     '.tardis_pytorch',
-                                     f'{m[0]}_{m[1]}',
-                                     'model_weights.pth'), 'rb'))
-            save_md5 = md5.hexdigest()
+            try:
+                save = json.load(open(join(expanduser('~'),
+                                           '.tardis_pytorch',
+                                           f'{model_name[0]}_{model_name[1]}',
+                                           f'{model_name[2]}',
+                                           'model_header.json')))
+            except:
+                save = None
 
-    elif len(m) == 3:
-        if not isfile(join(expanduser('~'),
-                           '.tardis_pytorch',
-                           f'{m[0]}_{m[1]}_{m[2]}',
-                           'model_weights.pth')):
-            return False
-        else:
-
-            md5 = MD5Count(open(join(expanduser('~'),
-                                     '.tardis_pytorch',
-                                     f'{m[0]}_{m[1]}_{m[2]}',
-                                     'model_weights.pth'), 'rb'))
-            save_md5 = md5.hexdigest()
-
-    aws_md5 = None
-    if save_md5 is None:
-        return False
+    """Compare stored file with file stored on aws"""
+    aws = None
+    if save is None:
+        return False  # Error loading json, download from aws
     else:
         try:
-            if len(m) == 2:
-                md5 = MD5Count(requests.get(
-                    f'https://tardis-weigths.s3.amazonaws.com/{m[0]}_{m[1]}/model_weights.pth',
-                    stream=True
-                ))
-                aws_md5 = md5.hexdigest()
-            elif len(m) == 3:
-                md5 = MD5Count(requests.get(
-                    f'https://tardis-weigths.s3.amazonaws.com/{m[0]}/{m[2]}/{m[1]}/model_weights.pth',
-                            stream=True
-                            ))
-                aws_md5 = md5.hexdigest()
+            weight = requests.get(
+                f'https://tardis-weigths.s3.amazonaws.com/{model_name[0]}_{model_name[1]}/'
+                f'{model_name[2]}/model_weights.pth',
+                stream=True
+            )
+            aws = dict(weight.headers)
         except:
-            return True
+            return True  # Found saved weight but cannot connect to aws
 
-    if save_md5 == aws_md5:
-        return True
+    if save['Last-Modified'] == aws['Last-Modified']:
+        return True  # Up-to data weight, load from local dir
     else:
-        return False
+        return False  # There is new version on aws, download from aws
