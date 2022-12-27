5496f206e958c653bbf7c345c2bbaa98327a6f94
Robert Kiewisz <56911280+RRobert92@users.noreply.github.com>
2022-12-27 13:56:24 +0100

    run all pytest on all python versions

--- run_tests.py
+++ run_tests.py
@@ -1,31 +1,26 @@
 import subprocess as subp
 import sys
-import pytest
 
 
 def main():
     if sys.platform != 'darwin':
-            ################
-            # PYTHON 3.7.* #
-            ################
-            # Set up Python 3.7 env and update
-            subp.run('conda run -n tardis37 conda update python -y', shell=True)
+        ################
+        # PYTHON 3.7.* #
+        ################
+        # Set up Python 3.7 env and update
+        subp.run('conda run -n tardis37 conda update python -y', shell=True)
 
-            # Check and reinstall if needed requirements
-            try:
-                subp.run('conda run -n tardis37 pip install -r requirements.txt', shell=True)
-                subp.run('conda run -n tardis37 pip install -r requirements-dev.txt', shell=True)
-                subp.run('conda run -n tardis37 conda clean -a -y', shell=True)
-                subp.run('conda run -n tardis37 pip cache purge', shell=True)
-            except:
-                print('Skipped conda and pip update!')
+        # Check and reinstall if needed requirements
+        subp.run('conda run -n tardis37 pip install -r requirements.txt', shell=True)
+        subp.run('conda run -n tardis37 pip install -r requirements-dev.txt', shell=True)
+        subp.run('conda run -n tardis37 conda clean -a -y', shell=True)
+        subp.run('conda run -n tardis37 pip cache purge', shell=True)
 
-            # Install tardis-pytorch
-            subp.run('conda run -n tardis37 pip install -e . -y', shell=True)
+        # Install tardis-pytorch
+        subp.run('conda run -n tardis37 pip install -e .', shell=True)
 
-            # Test on Python 3.7.*
-            subp.run('conda run -n tardis37 pytest', shell=True)
-
+        # Test on Python 3.7.*
+        subp.run('conda run -n tardis37 pytest', shell=True)
 
     ################
     # PYTHON 3.8.* #
@@ -34,20 +29,16 @@ def main():
     subp.run('conda run -n tardis38 conda update python -y', shell=True)
 
     # Check and reinstall if needed requirements
-    try:
-        subp.run('conda run -n tardis38 pip install -r requirements.txt', shell=True)
-        subp.run('conda run -n tardis38 pip install -r requirements-dev.txt', shell=True)
-        subp.run('conda run -n tardis38 conda clean -a -y', shell=True)
-        subp.run('conda run -n tardis38 pip cache purge', shell=True)
-    except:
-        print('Skipped conda and pip update!')
+    subp.run('conda run -n tardis38 pip install -r requirements.txt', shell=True)
+    subp.run('conda run -n tardis38 pip install -r requirements-dev.txt', shell=True)
+    subp.run('conda run -n tardis38 conda clean -a -y', shell=True)
+    subp.run('conda run -n tardis38 pip cache purge', shell=True)
 
     # Install tardis-pytorch
-    subp.run('conda run -n tardis38 pip install -e . -y', shell=True)
+    subp.run('conda run -n tardis38 pip install -e .', shell=True)
 
     # Test on Python 3.8.*
     subp.run('conda run -n tardis38 pytest', shell=True)
-
 
     ################
     # PYTHON 3.9.* #
@@ -65,11 +56,10 @@ def main():
         print('Skipped conda and pip update!')
 
     # Install tardis-pytorch
-    subp.run('conda run -n tardis39 pip install -e . -y', shell=True)
+    subp.run('conda run -n tardis39 pip install -e .', shell=True)
 
     # Test on Python 3.9.*
     subp.run('conda run -n tardis39 pytest', shell=True)
-
 
     #################
     # PYTHON 3.10.* #
@@ -87,7 +77,7 @@ def main():
         print('Skipped conda and pip update!')
 
     # Install tardis-pytorch
-    subp.run('conda run -n tardis310 pip install -e . -y', shell=True)
+    subp.run('conda run -n tardis310 pip install -e .', shell=True)
 
     # Test on Python 3.10.*
     subp.run('conda run -n tardis310 pytest', shell=True)
