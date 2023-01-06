5b4b546ce0b8a017b21d70c4e883238ee8285c0e
Robert Kiewisz <56911280+RRobert92@users.noreply.github.com>
2023-01-06 21:45:10 +0100

    bugfix

--- tardis/utils/setup_envir.py
+++ tardis/utils/setup_envir.py
@@ -108,11 +108,6 @@ def check_dir(dir: str,
     Returns:
         bool: Bool value indicating detection of the correct structure dataset
     """
-    if isinstance(img_format, str):
-        img_format = [img_format]
-    if isinstance(mask_format, str):
-        mask_format = [mask_format]
-
     dataset_test = False
     if isdir(join(dir, 'train')) and isdir(join(dir, 'test')):
         dataset_test = True
@@ -121,8 +116,8 @@ def check_dir(dir: str,
             # Check if train img and coord exist and have same files
             if isdir(train_img) and isdir(train_mask):
                 if len([f for f in listdir(train_img)
-                        if f.endswith(img_format)]) == len([f for f in listdir(train_mask)
-                                                            if f.endswith(mask_format)]):
+                        if f.endswith(img_format)]) == \
+                        len([f for f in listdir(train_mask) if f.endswith(mask_format)]):
                     if len([f for f in listdir(train_img)
                             if f.endswith(img_format)]) == 0:
                         dataset_test = False
