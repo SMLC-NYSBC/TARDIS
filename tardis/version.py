65686df2ff0bb36b9b0b1bfb07877333801903e5
Robert Kiewisz <56911280+RRobert92@users.noreply.github.com>
2023-03-29 10:48:31 +0200

    ghitub workflow update

--- tardis/version.py
+++ tardis/version.py
@@ -1,1 +1,11 @@
+# #####################################################################
+#  TARDIS - Transformer And Rapid Dimensionless Instance Segmentation #
+#                                                                     #
+#  New York Structural Biology Center                                 #
+#  Simons Machine Learning Center                                     #
+#                                                                     #
+#  Robert Kiewisz, Tristan Bepler                                     #
+#  MIT License 2021 - 2023                                            #
+# #####################################################################
+
 version = '0.1.0rc2+hotfix2'
