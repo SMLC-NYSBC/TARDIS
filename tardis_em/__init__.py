83f857034b805c76f7519ce2305c9a7c4154544f
Robert Kiewisz <56911280+RRobert92@users.noreply.github.com>
2023-10-06 16:08:43 -0400

    test up

--- tardis_em/__init__.py
+++ tardis_em/__init__.py
@@ -1,6 +1,6 @@
 from tardis_em._version import version as __version__
 
 # Uncomment on deployment
-# from tardis_em.utils.ota_update import ota_update
-#
-# ota = ota_update()
+from tardis_em.utils.ota_update import ota_update
+
+ota = ota_update()
