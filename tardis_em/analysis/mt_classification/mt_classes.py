a8303ed8db128dc4b098cf6ec23d834a0e9dd866
Robert Kiewisz <56911280+RRobert92@users.noreply.github.com>
2024-10-15 19:15:55 +0200

    added full entry point

--- tardis_em/analysis/mt_classification/mt_classes.py
+++ tardis_em/analysis/mt_classification/mt_classes.py
@@ -11,7 +11,7 @@ import numpy as np
 from sklearn.neighbors import NearestNeighbors
 from tardis_em.analysis.filament_utils import resample_filament, reorder_segments_id
 
-from analysis.mt_classification.utils import (
+from tardis_em.analysis.mt_classification.utils import (
     distances_of_ends_to_surface,
     count_true_groups,
     divide_into_sequences,
@@ -22,6 +22,7 @@ from analysis.mt_classification.utils import (
     assign_filaments_to_poles,
 )
 from tardis_em.utils.load_data import load_am_surf, ImportDataFromAmira
+from tardis_em.utils.logo import TardisLogo
 
 
 class MicrotubuleClassifier:
@@ -33,6 +34,7 @@ class MicrotubuleClassifier:
         pixel_size,
         gaps_size=100,
         kmt_dist_to_surf=1000,
+        tardis_logo=True,
     ):
         """
         Initializes the Microtubule Classifier with the necessary data.
@@ -46,16 +48,58 @@ class MicrotubuleClassifier:
             kmt_dist_to_surf (int): Distance of plus end MT to the surface in A
                 to be considered as KMT.
         """
+        self.tardis_logo = tardis_logo
+        if self.tardis_logo:
+            self.main_logo = TardisLogo()
+            self.main_logo(
+                title=f"| Transforms And Rapid Dimensionless Instance Segmentation",
+                text_1="WELCOME to TARDIS_em - analysis [MT classification]!",
+                text_2="Est. elapse time ~10 min",
+            )
+
         self.pixel_size = pixel_size
         self.gap_size = gaps_size
         self.kmt_dist_to_surf = kmt_dist_to_surf
         self.surfaces = surfaces
 
+        if self.tardis_logo:
+            self.main_logo(
+                title=f"| Transforms And Rapid Dimensionless Instance Segmentation",
+                text_1="WELCOME to TARDIS_em - analysis [MT classification]!",
+                text_2="Est. elapse time ~10 min",
+                text_3="Classifying MT - Loading data...",
+            )
+
         self.vertices = self.get_vertices_file(surfaces, simplify=None)
         self.filaments = self.get_filament_file(filaments)
         self.filament_pole1, self.filament_pole2 = None, None
         self.poles = self.get_poles_file(poles)
+        self.min_coords = None
+
+        if self.tardis_logo:
+            self.main_logo(
+                title=f"| Transforms And Rapid Dimensionless Instance Segmentation",
+                text_1="WELCOME to TARDIS_em - analysis [MT classification]!",
+                text_2="Est. elapse time ~10 min",
+                text_3="Classifying MT - Starting preprocessing...",
+            )
         self.correct_data()
+        self.f_1 = len(np.unique(self.filament_pole1[:, 0]))
+        self.f_2 = len(np.unique(self.filament_pole2[:, 0]))
+
+        if self.tardis_logo:
+            self.main_logo(
+                title=f"| Transforms And Rapid Dimensionless Instance Segmentation",
+                text_1="WELCOME to TARDIS_em - analysis [MT classification]!",
+                text_2="Est. elapse time ~10 min",
+                text_3="Classifying MT - Classifying...",
+                text_4=f"Filament_pole_1: {self.f_1} | Filament_pole_1: {self.f_2 }",
+                text_5="KMTs: NA",
+                text_6="Mid-MT: NA",
+                text_7="Interdigitating-MTs: NA",
+                text_8="Bridging-MTs: NA",
+                text_9=f"SMTs: NA",
+            )
 
         self.plus_end_id, self.minus_end_id = None, None
 
@@ -81,21 +125,23 @@ class MicrotubuleClassifier:
 
     def correct_data(self):
         """Correct the coordinates based on pixel size."""
-        min_coords = self.filaments[:, 1:].min(axis=0)
+        self.min_coords = self.filaments[:, 1:].min(axis=0)
 
         # Efficient normalization using broadcasting
-        self.filaments[:, 1:] = (self.filaments[:, 1:] - min_coords) / 25.70
+        self.filaments[:, 1:] = (self.filaments[:, 1:] -self. min_coords) / self.pixel_size
         self.filaments = resample_filament(self.filaments, 1)
 
         # Normalize vertices and poles
         for i in range(len(self.vertices)):
-            self.vertices[i] = (self.vertices[i] - min_coords) / self.pixel_size
+            self.vertices[i] = (self.vertices[i] - self.min_coords) / self.pixel_size
 
-        self.poles = ((self.poles - min_coords) / self.pixel_size).astype(np.int32)
+        self.poles = ((self.poles - self.min_coords) / self.pixel_size).astype(np.int32)
         self.poles = pick_pole_to_surfaces(self.poles, self.vertices)
 
         # Assign filaments to poles and reorder
-        self.filament_pole1, self.filament_pole2 = assign_filaments_to_poles(self.filaments, self.poles)
+        self.filament_pole1, self.filament_pole2 = assign_filaments_to_poles(
+            self.filaments, self.poles
+        )
         self.filament_pole1 = reorder_segments_id(self.filament_pole1)
         self.filament_pole2 = reorder_segments_id(self.filament_pole2)
         self.filament_pole2[:, 0] += np.max(self.filament_pole1[:, 0]) + 1
@@ -130,7 +176,9 @@ class MicrotubuleClassifier:
             return []
 
         # Calculate distances of MT endpoints to the surface and poles
-        d1_, d2_ = distances_of_ends_to_surface(self.vertices[id_], self.poles[id_], plus_end)
+        d1_, d2_ = distances_of_ends_to_surface(
+            self.vertices[id_], self.poles[id_], plus_end
+        )
 
         # Select MTs based on distance threshold
         dist_to_surf_th = d1_ <= (self.kmt_dist_to_surf / self.pixel_size)
@@ -308,34 +356,102 @@ class MicrotubuleClassifier:
         self.kmts_inside_id_1, self.kmts_outside_id_1 = self.kmts_inside_outside(
             self.filaments[np.isin(self.filaments[:, 0], self.kmts_id_1)], id_=0
         )
-        self.kmts_inside_id_1, self.kmts_outside_id_1 = list(self.kmts_inside_id_1), list(self.kmts_outside_id_1)
+        self.kmts_inside_id_1, self.kmts_outside_id_1 = list(
+            self.kmts_inside_id_1
+        ), list(self.kmts_outside_id_1)
         self.kmts_outside_id_2, self.kmts_inside_id_2 = self.kmts_inside_outside(
             self.filaments[np.isin(self.filaments[:, 0], self.kmts_id_2)], id_=1
         )
-        self.kmts_outside_id_2, self.kmts_inside_id_2 = list(self.kmts_outside_id_2), list(self.kmts_inside_id_2)
+        self.kmts_outside_id_2, self.kmts_inside_id_2 = list(
+            self.kmts_outside_id_2
+        ), list(self.kmts_inside_id_2)
+
+        if self.tardis_logo:
+            self.main_logo(
+                title=f"| Transforms And Rapid Dimensionless Instance Segmentation",
+                text_1="WELCOME to TARDIS_em - analysis [MT classification]!",
+                text_2="Est. elapse time ~10 min",
+                text_3="Classifying MT - Classifying...",
+                text_4=f"Filament_pole_1: {self.f_1} | Filament_pole_1: {self.f_2 }",
+                text_5=f"KMTs: {len(self.kmts_id_1)+len(self.kmts_id_2)}",
+                text_6="Mid-MT: NA",
+                text_7="Interdigitating-MTs: NA",
+                text_8="Bridging-MTs: NA",
+                text_9=f"SMTs: NA",
+            )
 
         # Select and assign mid-MTs, interdigitating-MTs, and bridging-MTs
         self.mid_mt_ids = self.assign_to_mid_mt()
+        if self.tardis_logo:
+            self.main_logo(
+                title=f"| Transforms And Rapid Dimensionless Instance Segmentation",
+                text_1="WELCOME to TARDIS_em - analysis [MT classification]!",
+                text_2="Est. elapse time ~10 min",
+                text_3="Classifying MT - Classifying...",
+                text_4=f"Filament_pole_1: {self.f_1} | Filament_pole_1: {self.f_2 }",
+                text_5=f"KMTs: {len(self.kmts_id_1)+len(self.kmts_id_2)}",
+                text_6=f"Mid-MT: {len(self.mid_mt_ids)}",
+                text_7="Interdigitating-MTs: NA",
+                text_8="Bridging-MTs: NA",
+                text_9=f"SMTs: NA",
+            )
+
         self.int_mt_ids = self.assign_to_int_mt()
+        if self.tardis_logo:
+            self.main_logo(
+                title=f"| Transforms And Rapid Dimensionless Instance Segmentation",
+                text_1="WELCOME to TARDIS_em - analysis [MT classification]!",
+                text_2="Est. elapse time ~10 min",
+                text_3="Classifying MT - Classifying...",
+                text_4=f"Filament_pole_1: {self.f_1} | Filament_pole_1: {self.f_2 }",
+                text_5=f"KMTs: {len(self.kmts_id_1)+len(self.kmts_id_2)}",
+                text_6=f"Mid-MT: {len(self.mid_mt_ids)}",
+                text_7=f"Interdigitating-MTs: {len(self.int_mt_ids)}",
+                text_8="Bridging-MTs: NA",
+                text_9=f"SMTs: NA",
+            )
+
         self.brg_mt_ids = self.assign_to_bridge_mt()
 
         """Select SMTs"""
-        self.smt_ids = self.kmts_id_1 + self.kmts_id_2 + self.mid_mt_ids + self.int_mt_ids + self.brg_mt_ids
+        self.smt_ids = (
+            self.kmts_id_1
+            + self.kmts_id_2
+            + self.mid_mt_ids
+            + self.int_mt_ids
+            + self.brg_mt_ids
+        )
         self.smt_fiber = self.filaments[~np.isin(self.filaments[:, 0], self.smt_ids)]
 
         self.smt_ids = list(np.unique(self.smt_fiber[:, 0]))
 
+        if self.tardis_logo:
+            self.main_logo(
+                title=f"| Transforms And Rapid Dimensionless Instance Segmentation",
+                text_1="WELCOME to TARDIS_em - analysis [MT classification]!",
+                text_2="Est. elapse time ~10 min",
+                text_3="Classifying MT - Classifying...",
+                text_4=f"Filament_pole_1: {self.f_1} | Filament_pole_1: {self.f_2}",
+                text_5=f"KMTs: {len(self.kmts_id_1) + len(self.kmts_id_2)}",
+                text_6=f"Mid-MT: {len(self.mid_mt_ids)}",
+                text_7=f"Interdigitating-MTs: {len(self.int_mt_ids)}",
+                text_8=f"Bridging-MTs: {len(self.brg_mt_ids)}",
+                text_9=f"SMTs: {len(self.smt_ids)}",
+            )
+
     def get_classified_indices(self):
         return [
-                [self.kmts_inside_id_1, self.kmts_outside_id_1],
-                [self.kmts_inside_id_2, self.kmts_outside_id_2],
-                self.mid_mt_ids,
-                self.int_mt_ids,
-                self.brg_mt_ids,
-                self.smt_ids,
-            ]
+            [self.kmts_inside_id_1, self.kmts_outside_id_1],
+            [self.kmts_inside_id_2, self.kmts_outside_id_2],
+            self.mid_mt_ids,
+            self.int_mt_ids,
+            self.brg_mt_ids,
+            self.smt_ids,
+        ]
 
     def get_classified_fibers(self):
+        self.filaments[:, 1:] = (self.filaments[:, 1:] + self.min_coords) * self.pixel_size
+
         kmt_fiber_inside_1 = self.filaments[
             np.isin(self.filaments[:, 0], self.kmts_inside_id_1)
         ]
@@ -354,13 +470,13 @@ class MicrotubuleClassifier:
         brg_mt_fiber = self.filaments[np.isin(self.filaments[:, 0], self.brg_mt_ids)]
 
         return [
-                [kmt_fiber_inside_1, kmt_fiber_outside_1],
-                [kmt_fiber_inside_2, kmt_fiber_outside_2],
-                mid_mt_fiber,
-                int_mt_fiber,
-                brg_mt_fiber,
-                self.smt_fiber,
-            ]
+            [kmt_fiber_inside_1, kmt_fiber_outside_1],
+            [kmt_fiber_inside_2, kmt_fiber_outside_2],
+            mid_mt_fiber,
+            int_mt_fiber,
+            brg_mt_fiber,
+            self.smt_fiber,
+        ]
 
     def get_filaments(self):
         return self.filaments
@@ -369,9 +485,10 @@ class MicrotubuleClassifier:
         if simplify is not None:
             return self.vertices, self.triangles
         else:
-            _, _, vertices, self.triangles = self.get_vertices_file(self.surfaces, simplify)
+            _, _, vertices, self.triangles = self.get_vertices_file(
+                self.surfaces, simplify
+            )
             return vertices, self.triangles
 
     def get_poles(self):
         return self.poles
-
--- tardis_em/scripts/tardis_analysis_mt_class.py
+++ tardis_em/scripts/tardis_analysis_mt_class.py
@@ -16,6 +16,7 @@ import os
 from tardis_em.analysis.mt_classification.mt_classes import MicrotubuleClassifier
 from tardis_em.utils.export_data import NumpyToAmira
 from tardis_em._version import version
+from tardis_em.utils.logo import TardisLogo
 
 
 @click.command()
@@ -54,7 +55,7 @@ from tardis_em._version import version
     type=int,
     default=100,
     help="Gap size to fill out. Assuming very uneven surface to help the tool estimate when MT"
-         "enter and exit the volume, the small gaps can be marge on the microtubule.",
+    "enter and exit the volume, the small gaps can be marge on the microtubule.",
     show_default=True,
 )
 @click.option(
@@ -69,17 +70,24 @@ from tardis_em._version import version
     "-v",
     "--visualize",
     type=bool,
-    default=1000,
+    default=False,
     help="MT (+) end distance to the NN surface which allow it be be assigned as KMTs..",
     show_default=True,
 )
 @click.version_option(version=version)
-def main(path_surface: str, path_filaments: str, path_poles: str,
-         pixel_size: float, gap_size: int, kmts_to_surface_distance: int,
-         visualize: bool):
+def main(
+    path_surface: str,
+    path_filaments: str,
+    path_poles: str,
+    pixel_size: float,
+    gap_size: int,
+    kmts_to_surface_distance: int,
+    visualize: bool,
+):
     dir_ = os.path.dirname(path_surface)
     out_ = os.path.join(dir_, "analysis")
     out_name = os.path.split(path_filaments)[-1][:-3] + "_classes.am"
+    out_name = os.path.join(dir_, "analysis", out_name)
 
     if not os.path.isdir(out_):
         mkdir(out_)
@@ -92,32 +100,36 @@ def main(path_surface: str, path_filaments: str, path_poles: str,
         gaps_size=gap_size,
         kmt_dist_to_surf=kmts_to_surface_distance,
     )
+
     classifier.classified_MTs()
 
     classes = classifier.get_classified_fibers()
 
-    am = NumpyToAmira(as_point_cloud=False,
-                      header=[
-                          f"Classification Analysis done with TARDIS_em v{version}",
-                          "Segmentation results - ",
-                          f"KMTs: {len(classes[0][0]) + len(classes[0][1]) + len(classes[1][0]) + len(classes[1][1])}",
-                          f"Mid-MTs: {len(classes[2])}",
-                          f"Interdigitating-MTs: {len(classes[3])}",
-                          f"Bridging-MTs: {len(classes[4])}",
-                          f"SMTs: {len(classes[5])}",
-                  ])
+    am = NumpyToAmira(
+        as_point_cloud=False,
+        header=[
+            f"Classification Analysis done with TARDIS_em v{version}",
+            "Segmentation results:",
+            f"KMTs: {len(classes[0][0]) + len(classes[0][1]) + len(classes[1][0]) + len(classes[1][1])}",
+            f"Mid-MTs: {len(classes[2])}",
+            f"Interdigitating-MTs: {len(classes[3])}",
+            f"Bridging-MTs: {len(classes[4])}",
+            f"SMTs: {len(classes[5])}",
+        ],
+    )
 
     am.export_amira(
         file_dir=out_name,
-        coords=[classes[5],
-                classes[0][0],
-                classes[0][1],
-                classes[1][0],
-                classes[1][1],
-                classes[2],
-                classes[3],
-                classes[4],
-                ],
+        coords=[
+            classes[5],
+            classes[0][0],
+            classes[0][1],
+            classes[1][0],
+            classes[1][1],
+            classes[2],
+            classes[3],
+            classes[4],
+        ],
         labels=[
             "SMTs",
             "KMTs_pole1_Inside",
@@ -126,26 +138,47 @@ def main(path_surface: str, path_filaments: str, path_poles: str,
             "KMTs_pole2_Outside",
             "Mid-MTs",
             "Interdigitating-MTs",
-            "Bridging-MTs"
+            "Bridging-MTs",
         ],
     )
 
     if visualize:
-        from tardis_em.utils.visualize_pc import VisualizeFilaments, VisualizeSurface, VisualizeCompose
-        vertices, triangles = classifier.get_vertices(simplify=128)
-        vis_kmt = VisualizeFilaments(np.vstack((np.vstack(classes[0]), np.vstack(classes[1]))),
-                                     False, filament_color="red", return_=True)
-        vis_mid_mt = VisualizeFilaments(classes[2], False, filament_color="magenta", return_=True)
-        vis_brg_mt = VisualizeFilaments(classes[3], False, filament_color="white", return_=True)
-        vis_int_mt = VisualizeFilaments(classes[4], False, filament_color="orange", return_=True)
-        vis_smt = VisualizeFilaments(classes[5], False, filament_color="yellow", return_=True)
-        surf = VisualizeSurface(vertices, triangles, False, return_=True)
+        try:
+            from tardis_em.utils.visualize_pc import (
+                VisualizeFilaments,
+                VisualizeSurface,
+                VisualizeCompose,
+            )
 
-        VisualizeCompose(False,
-                         vis_smt=vis_smt,
-                         vis_kmt=vis_kmt,
-                         vis_mid_mt=vis_mid_mt,
-                         vis_int_mt=vis_int_mt,
-                         vis_brg_mt=vis_brg_mt,
-                         surf=surf
-                         )
+            vertices, triangles = classifier.get_vertices(simplify=128)
+            vis_kmt = VisualizeFilaments(
+                np.vstack((np.vstack(classes[0]), np.vstack(classes[1]))),
+                False,
+                filament_color="red",
+                return_=True,
+            )
+            vis_mid_mt = VisualizeFilaments(
+                classes[2], False, filament_color="magenta", return_=True
+            )
+            vis_brg_mt = VisualizeFilaments(
+                classes[3], False, filament_color="white", return_=True
+            )
+            vis_int_mt = VisualizeFilaments(
+                classes[4], False, filament_color="orange", return_=True
+            )
+            vis_smt = VisualizeFilaments(
+                classes[5], False, filament_color="yellow", return_=True
+            )
+            surf = VisualizeSurface(vertices, triangles, False, return_=True)
+
+            VisualizeCompose(
+                False,
+                vis_smt=vis_smt,
+                vis_kmt=vis_kmt,
+                vis_mid_mt=vis_mid_mt,
+                vis_int_mt=vis_int_mt,
+                vis_brg_mt=vis_brg_mt,
+                surf=surf,
+            )
+        except ModuleNotFoundError:
+            pass
