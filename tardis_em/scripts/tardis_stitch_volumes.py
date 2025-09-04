9e3e405db993acf37d491c53066418d8aece441c
Robert Kiewisz <56911280+RRobert92@users.noreply.github.com>
2025-09-04 15:22:29 +0200

    license

---
+++ tardis_em/scripts/tardis_stitch_volumes.py
@@ -0,0 +1,9 @@
+#######################################################################
+#  TARDIS - Transformer And Rapid Dimensionless Instance Segmentation #
+#                                                                     #
+#  New York Structural Biology Center                                 #
+#  Simons Machine Learning Center                                     #
+#                                                                     #
+#  Robert Kiewisz, Tristan Bepler                                     #
+#  MIT License 2021 - 2025                                            #
+#######################################################################
